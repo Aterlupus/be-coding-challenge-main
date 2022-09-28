8bdb54a4282bf1f6a33d890c20739f6c71c0c65a
arczi <artsie122@gmail.com>
2022-09-29 01:43:42 +0200

    splitting testItImportsLogsFileToDatabase method

--- devbox/tests/Command/ImportLogsConsoleCommandIntegrationTest.php
+++ devbox/tests/Command/ImportLogsConsoleCommandIntegrationTest.php
@@ -8,6 +8,7 @@ use App\Entity\LogsEntry;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
 
 class ImportLogsConsoleCommandIntegrationTest extends KernelTestCase
@@ -24,23 +25,29 @@ class ImportLogsConsoleCommandIntegrationTest extends KernelTestCase
     public function testItImportsLogsFileToDatabase()
     {
         $commandObject = new ImportLogsConsoleCommand($this->entityManager, $this->getContainer()->get('command.bus'));
-
-        $kernel = self::bootKernel();
-        $application = new Application($kernel);
-        $application->add($commandObject);
-
-        $arguments = ['filepath' => 'logs/logs.txt'];
-        $inputs = [];
-        $command = $application->find('app:import-logs');
-        $commandTester = new CommandTester($command);
-        $commandTester->setInputs($inputs);
+        $commandTester = $this->getCommandTester($commandObject);
 
         self::assertEquals(0, $this->entityManager->getRepository(LogsEntry::class)->count([]));
 
-        $commandTester->execute($arguments);
+        $commandTester->execute(['filepath' => 'logs/logs.txt']);
 
         self::assertEquals(20, $this->entityManager->getRepository(LogsEntry::class)->count([]));
         $this->entityManager->getConnection()->prepare('DELETE FROM logs_entry WHERE 1=1')->executeStatement(); //TODO: Discover why beginTransaction/rollback method doesn't work and replace with safer solution
         $this->entityManager->getConnection()->prepare('DELETE FROM logs_import WHERE 1=1')->executeStatement();
+    }
+
+    private function getCommandTester(
+        Command $commandObject,
+        array $inputs = []
+    ): CommandTester {
+        $kernel = self::bootKernel();
+        $application = new Application($kernel);
+        $application->add($commandObject);
+
+        $command = $application->find('app:import-logs');
+        $commandTester = new CommandTester($command);
+        $commandTester->setInputs($inputs);
+
+        return $commandTester;
     }
 }
